628beb1f0f051d458744ea96438d4e7a597efc97
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-11-13 18:05:45 +0100

    ok

--- groupe2/web-service-v1/app.js
+++ groupe2/web-service-v1/app.js
@@ -1,4 +1,5 @@
 const http = require("http")
+const fs = require("fs")
 const PORT = 3000
 
 // configuration
@@ -12,13 +13,58 @@ const server = http.createServer((req, res) => {
     // http://domain-name||ip adress:PORT/path?clef=valeur&clef=valeur
     //exemple: on veut envoyer le nom mehdi et l age 32 au serveur : localhost:3000/ok
     // http://localhost:3000/ok?nom=mehdi&age=32
-    else if(method=="PUT" && url.startsWith("/n?")){ // /n?ok=10&number=20
+    else if (method == "PUT" && url.startsWith("/n?")) { // /n?ok=10&number=20
         let params = new URLSearchParams(url.split("?")[1])
         let number = params.get('number')
         res.write("number est")
         res.write(number)
         res.end()
-    }   
+    }
+    else if (method == "POST" && url.startsWith("/ok/")) {
+        let number = url.split("/")[2]
+        fs.promises.readFile('./db.json')
+            .then(data => data.toString())
+            .then(dataJSON => JSON.parse(dataJSON))
+            .then(data => {
+                let resultat = data.racines.find(ele => ele.number == number)// retourn soit objet soit undefined
+                if (!resultat) {
+                    res.statusCode = 404
+                    res.setHeader("Content-Type", "application/json")
+                    res.write(JSON.stringify({
+                        msg: "cannot delete number",
+                        reason: "number not found",
+                        number
+                    }))
+                    res.end()
+                }
+                else {
+                    data.racines = data.racines.filter(ele => ele != resultat)
+                    data.count--
+                    fs.promises.writeFile("./db.json", JSON.stringify(data,null,3))
+                        .then(() => {
+                            res.statusCode = 200
+                            res.setHeader("Content-Type", "application/json")
+                            res.write(JSON.stringify({
+                                msg: "number deleted with success",
+                                number
+                            }))
+                            res.end()
+                        })
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                res.statusCode = 500
+                res.setHeader("Content-Type", "application/json")
+                res.write(JSON.stringify({
+                    msg: "server problem !"
+                }))
+                res.end()
+            })
+        /*res.write("delete le nombre ")
+        res.write(number)
+        res.end()*/
+    }
     else {
         res.write("404")
         res.end()
