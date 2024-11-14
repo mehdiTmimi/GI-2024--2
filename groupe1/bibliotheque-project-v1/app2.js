6be2a6b6f57613d09d1a7af7a1d67c8167419e3f
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-11-14 16:11:38 +0100

    ok

--- groupe1/bibliotheque-project-v1/app2.js
+++ groupe1/bibliotheque-project-v1/app2.js
@@ -3,18 +3,25 @@ const PORT1 = 3000
 
 const server = http.createServer((req, res) => {
     const { url, method } = req
-    if(method=="GET" && url == "/es"){
+    if (method == "GET" && url == "/es") {
         res.write("list users")
         return res.end()
     }
     // http://localhost:3000/a/cd123 , http://localhost:3000/a/oook
-    if((method=="POST" || method=="PUT" ) && url.startsWith("/a/"))
-        {
-            let id = url.split("/")[2]
-            res.write(`user id : ${id}`)
-            return res.end()
+    if ((method == "POST" || method == "PUT") && url.startsWith("/a/")) {
+        let id = url.split("/")[2]
+        res.write(`user id : ${id}`)
+        return res.end()
     }
-    res.write("404")
+    res.statusCode = 404
+    //res.setHeader("Content-Type","text/html")
+    //res.write(`<h1 style="color:red"> 404 not found</h1>`)
+    let msgToSend = {
+        msg: "contenu introuvable",
+        path: url
+    }
+    res.setHeader("Content-Type","application/json")
+    res.write(JSON.stringify(msgToSend))
     res.end()
 })
 
